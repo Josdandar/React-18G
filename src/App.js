64837bac54bf3d023631013819bce8baafb8c656
Jose De Anda <josdandar@gmail.com>
2022-09-29 23:51:46 -0700

    ON OFF BOX useState

--- src/App.js
+++ src/App.js
@@ -7,44 +7,28 @@ import FullName from './components/FullName';
 
 function App() {
 
-  const handleClick = ( increase ) => {
-    let newCount = count
-    if(increase){
-      newCount = newCount + 1
+  const handleClick = ( switchStatus ) => {
+    if(switchStatus === "OFF"){
+      setswitchStatus("ON")
     }else{
-      newCount = newCount - 1
+      setswitchStatus("OFF")
     }
-    setCount(newCount)
   }
  
-  const [count, setCount] = useState(0)
-  //useEffect controla el ciclo de vida de los componentes en los componentes como funcion 
-  //useEffect argumentos
-  //ARGUMENTO 1: CALLBACK
-  //ARGUMENTO 2: [], NADA, [COUNT]
-  //En caso vacio useeffect se modificara cada que algo cambie (estado) 
-  // Caso []: el callback useEffect se ejecutara solo la primera vez que carga el componente 
-  //Caso [state]: el callback useEffect se ejecutara solo la primera vez y cada que se actualice el estado que se incluye
-  useEffect( ()=> {
-    console.log("SOLO SE EJECUTA LA PRIMERA VEZ QUE EL COMPONENTE SE CARGA ")
-  }, [])
+  const [switchStatus, setswitchStatus] = useState("OFF")
+  
+  const statusClass = switchStatus === "OFF"? "statusOff" : "statusOn";
 
-  useEffect( ()=> {
-    console.log("ME EJECUTO LA PRIMERA VEZ QUE EL COMPONENTE SE CARGA Y CADA VEZ QUE EL ESTADO SE ACTUALIZA")
-  }, [count])
-
-  useEffect( ()=> {
-    console.log("ME EJECUTO LA PRIMERA VEZ Y SIEMPRE QUE HAYA UNA ACTUALIZACION DEL COMPONENTE")
-  })
+  
 
   return (
     <div className="App">
       <header className="App-header">
         <Title text="Hi i am a title"/>
         <FullName firstName="Jose" lastName="Rivera"/>
-        <p>Counter: {count} veces</p>
-        <button onClick={()=> handleClick(true)}> + </button>
-        <button onClick={()=>handleClick(false)}> - </button>
+        
+        <button onClick={()=> handleClick(switchStatus)}> + </button>
+        <div className={statusClass}>{switchStatus}</div>
       </header>
     </div>
   );
